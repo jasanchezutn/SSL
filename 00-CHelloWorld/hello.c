b869d2bc7ec8209b8a9bd487c6c4bf979e1532c9
Javier Sanchez <37815931+jasanchezutn@users.noreply.github.com>
2018-04-02 21:07:09 -0300

    Add files via upload
    Upload del archivo fuente y de la salida del ejecutable exportada en txt.

---
+++ 00-CHelloWorld/hello.c
@@ -0,0 +1,16 @@
+/**
+  * \file hello.c
+  * \brief TP#0 - Programa que muestra por pantalla "Hello, World"
+  * \author Sánchez, Javier
+  * \date 2018.04.02
+  * \version 1.0
+ */
+
+#include <stdio.h>
+
+int main (void)
+{
+  printf("Hello, World!\n");
+  
+  return(0);
+};
